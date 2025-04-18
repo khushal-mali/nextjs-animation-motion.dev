3ebe7f72c19b2cdece8e887e698e8d8b714eeff4
Khushal Mali <khushalmali8448@gmail.com>
2025-04-18 15:23:31 +0530

    transformation

---
+++ src/app/01-Transformations/page.tsx
@@ -0,0 +1,26 @@
+import * as motion from "motion/react-client";
+
+const Home = () => {
+  return (
+    <div>
+      <motion.div className="box" animate={{ x: 200 }} />
+
+      <motion.div className="box" animate={{ y: 0 }} />
+
+      <motion.div className="box" animate={{ rotateX: 60 }} />
+      <motion.div className="box" animate={{ rotateY: 60 }} />
+
+      <motion.div className="box" animate={{ rotate: 20 }} />
+
+      <motion.div className="box" animate={{ scaleX: 2 }} />
+      <motion.div className="box" animate={{ scaleY: 2 }} />
+      <motion.div className="box" animate={{ scale: 2 }} />
+
+      <motion.div className="box" animate={{ skewX: 20 }} />
+      <motion.div className="box" animate={{ skewY: 20 }} />
+      <motion.div className="box" animate={{ skew: 20 }} />
+    </div>
+  );
+};
+
+export default Home;
